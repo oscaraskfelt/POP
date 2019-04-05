f0936f9210129cdd252f2dfc81ef0df9cc28c721
Oscar Askfelt <oscaraskfelt@gmail.com>
2019-04-05 16:45:12 +0200

    Börjat lite smått på databasänden också

--- POPhtml/app.py
+++ POPhtml/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, render_template
 app = Flask("POPhtml", static_url_path='/static')
 app = Flask(__name__.split('.')[0])
+import example
 
 @app.route('/')
 def sign_in():
@@ -14,13 +15,17 @@ def calendar():
 def test():
     return render_template('_new_task.html')
 
-@app.route('/new_task', methods=["POST"])
+@app.route('/new_task', methods=["POST", "GET"])
 def get_data():
-    dic = {}
-    task_title = request.form['task_head']
-    task_content = request.form['task_content']
-    #task_prio = request.form['task_prio']
-    #task_date = request.form['new_task_date']
+    if request.method == "POST":
+        dic = {}
+        #task_title = request.form['task_head']
+        #task_content = request.form['task_content']
+        #task_prio = request.form['task_prio']
+        #task_date = request.form['new_task_date']
+        data = example.get_tasks()
+        return render_template('test.html', lista=data)
+    elif request.method =="GET":
+        return render_template('_new_task.html')
 
-    return render_template('test.html', lista=data)
 
