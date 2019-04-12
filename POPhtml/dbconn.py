a75d39d55adce8185c607bdd32b31ecdcb882da5
Birger Norrman <birger.norrman@gmail.com>
2019-04-12 09:56:43 +0200

    påbörjat registreringskod

--- POPhtml/dbconn.py
+++ POPhtml/dbconn.py
@@ -1,3 +1,4 @@
+# coding: utf-8 
 import psycopg2
 
 def get_tasks():
--- POPhtml/reg_user.py
+++ POPhtml/reg_user.py
@@ -1,5 +1,32 @@
-def reg_user():
+import psycopg2
+
+def get_user():
+    ''' hämtar alla användare från db'''
+    conn = psycopg2.connect(dbname='pop', user='ai8812', password='wtrikq2c', host='pgserver.mah.se')
+    cursor = conn.cursor()
+    cursor.execute("select epost from popper")
+    epost = cursor.fetchall()
+    cursor.close()
+    return epost
+
+def epost_validation(epost):
+    ''''kontrollerar att epostadressen inte är upptagen'''
+    reg = request.form['reg']
+    for e in reg:
+        if e == epost:
+            pass
+        
+
+
+
+def reg_user(epost, losenord):
     '''Registrerar användare'''
 
     conn = psycopg2.connect(dbname='pop', user='ai8812', password='wtrikq2c', host='pgserver.mah.se')
-    cursor = conn.cursor("")
+    cursor = conn.cursor()
+    cursor.execute('''INSERT INTO popper (epost, popper_name, losenord)
+                        VALUES ('{}', '{}', '{}' )'''.format(epost, popper_name, losenord))
+    conn.commit()
+
+
+print(get_user())
