79dd62ade636a728224b68345e08355df0532f57
Oscar Askfelt <oscaraskfelt@gmail.com>
2019-04-24 14:37:16 +0200

    sorterade efter tid i timeline

--- POPhtml/dbconn.py
+++ POPhtml/dbconn.py
@@ -11,18 +11,16 @@ def get_tasks():
     return data
 
 
-def add_task(title, content, prio, date, enddate, user):
 def get_tasks_per_user(popper):
     '''Hämtar task från databasen för specifika användare'''
     conn = psycopg2.connect(dbname='pop', user='ai8812', password='wtrikq2c', host='pgserver.mah.se')
     cursor = conn.cursor()
-    cursor.execute("select * from task where popper = '{}'".format(popper))
+    cursor.execute("select * from task where popper = '{}' order by slutdatum".format(popper))
     data = cursor.fetchall()
     cursor.close()
     return data
 
 
-def add_task(title, content, prio, date, enddate):
 def add_task(title, content, prio, date, enddate, user):
     '''sparar ner datan till db'''
     conn = psycopg2.connect(dbname='pop', user='ai8812', password='wtrikq2c', host='pgserver.mah.se')
