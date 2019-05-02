039ba14fe660aee12fa55f5b00706ca0c4390369
Oscar Askfelt <oscaraskfelt@gmail.com>
2019-05-02 18:56:59 +0200

    default datumvärde till task

--- POPhtml/static/JS/task.js
+++ POPhtml/static/JS/task.js
@@ -1,4 +1,11 @@
-$('#plus').on('click', function(){
+$('.plus').on('click', function(){
     $("#task_form").toggleClass('visible');
-    $('#plus').toggleClass('rotate')
+    $('.plus').toggleClass('rotate');
 });
+
+var fullDate = new Date()
+    var twoDigitMonth = ((fullDate.getMonth().length+1) === 1)? (fullDate.getMonth()+1) : '0' + (fullDate.getMonth()+1);
+    var twoDigitDay = ((fullDate.getDay().length))? (fullDate.getDay()) : '0' + (fullDate.getDay() - 2);
+    var currentDate = fullDate.getFullYear() + "-" + twoDigitMonth + "-" + twoDigitDay;
+
+    $('#new_task_enddate').attr("value", currentDate);
