7a1fa2c4a33134e1a67a89843f7eb661228176e4
Birger Norrman <birger.norrman@gmail.com>
2019-05-24 23:29:24 +0200

    lagt till en nål som poppar ballongen

--- POPhtml/static/JS/balloons.js
+++ POPhtml/static/JS/balloons.js
@@ -1,6 +1,15 @@
 $(".ta_bort").click(function() {
+    $(this).parents("form").parents("div").siblings("#poppin").animate({left: '115%'});
     $(this).parents("form").parents("div").siblings(".btn").hide();
     $(this).parents("form").parents("div").siblings("#popcircle").hide("explode", {
         pieces: 50
     }, 250);
+  });
+
+$(".ta_bort").hover(function() {
+    $(this).parents("form").parents("div").siblings("#poppin").animate({left: '123%', opacity: '1',});
+  });
+
+$(".ta_bort").mouseout(function() {
+    $(this).parents("form").parents("div").siblings("#poppin").animate({left: '140%', opacity: '0',});
   });
